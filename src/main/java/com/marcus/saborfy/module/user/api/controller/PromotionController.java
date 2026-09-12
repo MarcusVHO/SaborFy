ba28a4489c7f3bf72f4ab43f4ac01878acacd39f
marcus <marcushilario2006@gmail.com>
2026-09-12 16:07:27 -0300

    feat: add role route created

---
+++ src/main/java/com/marcus/saborfy/module/user/api/controller/PromotionController.java
@@ -0,0 +1,4 @@
+package com.marcus.saborfy.module.user.api.controller;
+
+public class PromotionController {
+}
---
+++ src/main/java/com/marcus/saborfy/shared/exception/ForbiddenOperationException.java
@@ -0,0 +1,7 @@
+package com.marcus.saborfy.shared.exception;
+
+public class ForbbidenOperationException extends RuntimeException {
+  public ForbbidenOperationException(String message) {
+    super(message);
+  }
+}
